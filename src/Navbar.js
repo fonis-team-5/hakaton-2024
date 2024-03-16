023ffccb16e3f81bf46b42dd8a5eed3a7c07be4c
jelisavac-l <jelisavacluka63@gmail.com>
2024-03-16 22:56:44 +0100

    pre css

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,16 @@
 function Navbar() {
-    return(
-        <div className="navbar">
-        
-        </div>
-    );
+  return (
+    <div className="navbar">
+      <div className="left">
+        <h3>Lorem ipsum</h3>
+        <a href="#">Home</a>
+        <a href="#">Rewards</a>
+        <div className="right"></div>
+
+        <a href="#">Log in / Sign up</a>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
