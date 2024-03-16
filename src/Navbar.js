9b907567eea5ce092a51e8b29a5e190e3ce518d4
jelisavac-l <jelisavacluka63@gmail.com>
2024-03-16 23:18:52 +0100

    obojen navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,14 +2,15 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="left">
-        <h3>Lorem ipsum</h3>
-        <a href="#">Home</a>
-        <a href="#">Rewards</a>
-        <div className="right"></div>
-
-        <a href="#">Log in / Sign up</a>
-      </div>
+            <h3>CaringCoin  </h3>
+            <a href="#">Home</a>
+            <a href="#">Rewards</a>
+        </div>
+        <div className="right">
+            <a href="#">Log in / Sign up</a>
+        </div>
     </div>
+    
   );
 }
 
