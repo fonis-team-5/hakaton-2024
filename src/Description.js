558aeda8743e96da4d911b729fef54475c38e17f
jelisavac-l <jelisavacluka63@gmail.com>
2024-03-17 00:15:47 +0100

    Modal + nav filter

--- src/Description.js
+++ src/Description.js
@@ -1,9 +1,9 @@
 function Description() {
   return (
     <div className="description-container">
-      <h1> lorem ipsum kurac palac </h1>
+      <h1> CaringCoin </h1>
       <p id="OPIS">
-        Dobrodošli u prvu humanitarnu kockarnicu! Cilj naše aplikacije je da uz
+        Dobrodošli u prvu humanitarnu kockarnicu!<br></br>Cilj naše aplikacije je da uz
         adrenalinsko uzbuđenje igara na sreću učinimo nešto lepo, a takođe
         omogućimo našim korisnicima da osvoje vredne nagrade.
       </p>
