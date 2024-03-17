e835a4ffaca4e7ee0ebddb7dac0e508cc115d361
jelisavac-l <jelisavacluka63@gmail.com>
2024-03-17 02:01:07 +0100

    Svasta

--- src/Slot.js
+++ src/Slot.js
@@ -1,11 +1,15 @@
-function Slot() {
-    return(
-        <div className="card card-slot">
-            <h2>Slot</h2>
-            <img src=""></img>
+function Slot({ imageUrl, title, description }) {
+  return (
+    // <div className="card-gpt card-gpt-slot">
+      <div className="card-gpt">
+        <img src={imageUrl} alt={title} />
+        <div className="card-gpt-content">
+          <h2>{title}</h2>
+          <p>{description}</p>
         </div>
-    );
-
+      </div>
+    // </div>
+  );
 }
 
 export default Slot;
